6e8c89a12cf89ff0de8f6578aa60d5a09ca86c42
toto <tototianhao@gmail.com>
2024-09-15 02:19:49 +0800

    Auto update

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import numpy as np
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import JSONResponse
 import uvicorn
 
-from python_ai_utils.image_utils.image_convert import file_to_ndarray, file_to_cv2_image
-from python_ai_utils.model_scripts.nsfw_script import predict_simge_image, check_and_download_nsfw_model
+from python_ai_utils.image_utils.image_convert import byte_to_cv2_image, byte_to_ndarray
+from python_ai_utils.model_scripts.nsfw_script import predict_simge_image, check_and_download_nsfw_model, \
+    process_cv_images
 from python_ai_utils.model_scripts.sample_face_detection import is_single_face
 
 app = FastAPI()
@@ -11,9 +13,10 @@ app = FastAPI()
 
 @app.post("/ai/audit")
 async def upload_image(file: UploadFile = File(...)):
-    image_array = file_to_ndarray(file)
-    cv_image = file_to_cv2_image(file)
-    nsfw_info = predict_simge_image(image_array)
+    image_bytes = file.file.read()
+    cv_image = byte_to_cv2_image(image_bytes)
+    image_batch = process_cv_images([cv_image])
+    nsfw_info = predict_simge_image(image_batch)
     is_single = is_single_face(cv_image)
     return JSONResponse(content={"nsfw_info": nsfw_info, "is_single_face": is_single})
 
