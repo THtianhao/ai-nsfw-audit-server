09b6113f117f71858d075f7078bf47920d4e098c
toto <tototianhao@gmail.com>
2024-09-15 02:49:08 +0800

    Auto update

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
+import os.path
+
 import cv2
-import numpy as np
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import JSONResponse
 import uvicorn
 
+from env import root_path
 from python_ai_utils.image_utils.image_convert import byte_to_cv2_image, byte_to_ndarray, byte_to_pil_image
 from python_ai_utils.model_scripts.nsfw_script import predict_simge_image, check_and_download_nsfw_model, \
     process_cv_images
 from python_ai_utils.model_scripts.sample_face_detection import is_single_face
 from python_ai_utils.utils.fastapi_exception import error_in_file
-from python_ai_utils.utils.log_util import log_util
+from python_ai_utils.utils.uvicorn_log_util import get_uvicorn_log_config, log_uv
 
 app = FastAPI()
-error_in_file(app, log_util, "https://open.feishu.cn/open-apis/bot/v2/hook/f584024c-7c12-44d2-9407-e187fbc4387e")
+error_in_file(app, log_uv, "https://open.feishu.cn/open-apis/bot/v2/hook/f584024c-7c12-44d2-9407-e187fbc4387e", "nsfw")
 
 
 @app.post("/ai/audit")
 async def upload_image(file: UploadFile = File(...)):
+    raise 'test'
     image_bytes = file.file.read()
     cv_image = byte_to_cv2_image(image_bytes)
     cv_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB)
@@ -28,4 +31,9 @@ async def upload_image(file: UploadFile = File(...)):
 
 if __name__ == "__main__":
     check_and_download_nsfw_model()
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app,
+                host="0.0.0.0",
+                port=8000,
+                log_level="debug",
+                access_log=True,
+                log_config=get_uvicorn_log_config(os.path.join(root_path, "logs")), )
