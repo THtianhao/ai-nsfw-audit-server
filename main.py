34634eb30171fabb5dba76a050c92a5c5432bf2b
toto <tototianhao@gmail.com>
2024-09-29 00:31:21 +0800

    Auto update

--- main.py
+++ main.py
@@ -35,6 +35,7 @@ async def ping():
 if __name__ == "__main__":
     check_and_download_nsfw_model()
     uvicorn.run(app,
+
                 host="0.0.0.0",
                 port=8000,
                 log_level="debug",
